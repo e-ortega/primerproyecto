042b276951134972bc92f5c0752eaba073a9ff2e
Esteban Ortega Acuna <eortega1793@gmail.com>
2026-05-26 19:20:14 -0600

    Adding logic for cursos controller

---
+++ src/main/java/com/ufide/primerproyecto/CursosController.java
@@ -0,0 +1,24 @@
+package com.ufide.primerproyecto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CursosController {
+
+    @GetMapping("cursos")
+    public String ListarCursos(Model modelo) {
+        modelo.addAttribute("cursos", new String[] { "Java", "Python", "JavaScript" });
+        return "cursos";
+    }
+
+    @GetMapping("cursos/{cursoId}")
+    public String getMethodName(Model modelo, @PathVariable String cursoId) {
+        modelo.addAttribute("cursoId", cursoId);
+        return "curso";
+    }
+
+}
